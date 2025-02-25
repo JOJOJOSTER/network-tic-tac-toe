1c18422a7a782eef47bcfc690131db1b75e37100
jojo <musor2298@mail.ru>
2025-02-26 01:37:22 +0300

    Delete commented code

--- network-tic-tac-toe/src/network-tic-tac-toe.cpp
+++ network-tic-tac-toe/src/network-tic-tac-toe.cpp
@@ -1,16 +1,6 @@
 #include "../include/network-tic-tac-toe-game.h"
 #include <iostream>
 
-/*
-
-    1|2|3
-    -*-*-
-    4|5|6
-    -*-*-
-    7|8|9
-
-*/
-
 int main() {
 
   std::cout << "Hello world!" << std::endl;
@@ -20,17 +10,6 @@ int main() {
   if (!game.game()) {
     std::cout << "No one win" << std::endl;
   }
-
-  /*
-    Map map;
-    map.clearMap('X');
-    map.setSymbolToMap(1, 1, 'O');
-
-    printSquareMap(map);
-    std::cout << std::endl;
-    printSquareMapWithSeparators(map);
-
-  */
 
   return 0;
 }
